2903ef52ea9595b54c8b2fec68430d1c017ff1a3
Alexandre Antonino Gonçalves Martinazzo <alexandremartinazzo@gmail.com>
2019-06-10 17:12:03 -0300

    django 2 adjustments (also include admin access)

--- sp/urls.py
+++ sp/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls import (
-    include,
-    url,
-)
 from django.contrib.auth.decorators import login_required
+from django.contrib import admin
 from django.views.generic import TemplateView
+from django.urls import include, path
 
 
 urlpatterns = [
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^saml2/', include('djangosaml2.urls')),
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
-    url(
-        r'^protected-resource/',
-        login_required(TemplateView.as_view(template_name="protected.html"))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('saml2/', include('djangosaml2.urls')),
+    path('', TemplateView.as_view(template_name="index.html")),
+    path(
+        'protected-resource/',
+        login_required(TemplateView.as_view(template_name="protected.html")),
+        name='protected',
     ),
+    path('admin', admin.site.urls),
 ]
